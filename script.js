c286eed37237aa6e9337802ec56dd3388b87cf9c
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-10-26 22:57:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function getFormvalue() {
-    //Write your code here
-	alert(input[0],input[1]);
+     const firstName = document.getElementById("form1").elements.fname.value;
+  const lastName = document.getElementById("form1").elements.lname.value;
+
+  // Display the first and last name using an alert
+  alert( firstName +" " + lastName);
+
 
 }
